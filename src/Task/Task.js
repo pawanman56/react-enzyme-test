7dd73a5e87720ef8baa5a83d2a26c112f5a6f274
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-03-30 01:18:56 +0545

    props test

---
+++ src/Task/Task.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Task = (props) => <li id={props.id}>{ props.name }</li>;
+
+export default Task;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Task from './Task/Task';
 
 const TodoList = (props) => {
   return (
     <ul>
       {
-        props.tasks.map((taskName, index) =>
-          <li key={ index }>{ taskName }</li>
+        props.tasks.map(task =>
+          <Task key={ task.id } id={ task.id } name={ task.name } />
         )
       }
     </ul>
