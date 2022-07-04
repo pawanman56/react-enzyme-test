ed1ef24dd2894f98153f2e1761f2131dae1939cd
Pawan Sunuwar <pawansunuwar@Pawan-3.local>
2022-07-04 17:53:45 +0545

    cucumber before hook

--- cypress/integration/Search.feature
+++ cypress/integration/Search.feature
@@ -1,4 +1,7 @@
 Feature: Search funcationality
+    This feature allows searching through articles using a string
+
+    @homepage
     Scenario: Using the search input
         Given I am on the homepage
         When I fill the search input with the "JavaScript" term
--- cypress/integration/Search/Search.js
+++ cypress/integration/Search/Search.js
@@ -1,17 +1,5 @@
-import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
+import { Before } from 'cypress-cucumber-preprocessor/steps';
 
-Given('I am on the homepage', () => {
+Before({ tags: '@homepage' }, () => {
     cy.visit('');
-});
-
-When('I fill the search input with the {string} term', (term) => {
-    cy.get('.search-field').type(term);
-});
-
-And('I click on the submit button', () => {
-    cy.get('.search-submit').click();
-});
-
-Then('I should be redirected to a search page with the results of the {string} search', (term) => {
-    cy.get('.content-box .post-title--archive span').should('contain.text', term);
 });
